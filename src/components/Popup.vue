e4926841bc2fe865855e88fd76bc82510f3e72af
etienne1992 <etienne.rallu@heig-vd.ch>
2019-04-04 09:04:49 +0200

    PopUp à base de popopopop

---
+++ src/components/Popup.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="modal-card" style="width: auto">
+    <header class="modal-card-head">
+      <p class="modal-card-title">{{$t("popupPages." + popupRef +"."+ popupPage +".title")}}</p>
+      <p class="modal-card-subtitle">{{$t("popupPages." + popupRef +"."+ popupPage +".subTitle")}}</p>
+    </header>
+    <section class="modal-card-body">{{$t("popupPages." + popupRef +"."+ popupPage +".mainText")}}
+    <img src="../assets/logo-birdlife.png">
+    
+    
+    </section>
+    <template v-if="popupPage === 'problem'">
+    <footer class="modal-card-foot">
+      <button @click="loadSolution" class="button is-primary">{{$t("popupPages.learnMore")}}</button>
+    </footer>
+    </template>
+  </div>
+</template>
+<script>
+export default {
+  name: "Popup",
+  props: {
+    popupRef: String,
+    popupImages:String
+  },
+  data() {
+    return {
+      popupPage: "problem"
+    };
+  },
+  methods: {
+    // This method is called everytime the user click on the buttont that shows the solution to a trap<<
+    loadSolution() {
+      this.popupPage = "solution";
+    }
+  }
+};
+</script>
+<style scoped>
+</style>
--- src/views/Quizz.vue
+++ src/views/Quizz.vue
@@ -1,24 +1,46 @@
 <template>
   <div class="quizz">
-      <h1> {{ $t("quizzPage.title") }}</h1>
-      <p> {{$t("quizzPage.description")}}</p>
-      <router-link to="/register">{{ $t("quizzPage.link-to-register")}}</router-link> 
+    <h1>{{ $t("quizzPage.title") }}</h1>
+    <p>{{$t("quizzPage.description")}}</p>
+
+    <button class="button is-primary is-medium" @click="openPopUp('chimney')">Ouvrir Pop Up</button>
+
+    <b-modal :active.sync="isPopupActive" has-modal-card>
+      <Popup :popupRef=popupRef :popupImages=popupImages></Popup>
+    </b-modal>
+
+    <router-link to="/register">{{ $t("quizzPage.link-to-register")}}</router-link>
   </div>
 </template>
 
 <script>
+import Popup from "../components/Popup";
 export default {
-  name: 'Quizz',
-  props: {
-    msg: String
+  name: "Quizz",
+  props: {},
+  components: {
+    Popup
+  },
+  data() {
+    return {
+      isPopupActive: false,
+      popupRef: String,
+      popupImages : "../assets/logo-birdlife.png"
+    };
+  },
+  methods: {
+    openPopUp(ref) {
+      this.isPopupActive = true;
+      this.popupRef = ref;
+    }
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-.quizz{
-  background-color:green;
-  height:100%;
+.quizz {
+  background-color: green;
+  height: 100%;
 }
 </style>
