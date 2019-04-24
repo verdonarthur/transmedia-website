49b463aafd5fcacdb351124578a5ebd04daa717a
Arthur <verdon.arthur@gmail.com>
2019-04-24 22:55:31 +0200

    correction for mandator

--- src/views/RegisterConfirm.vue
+++ src/views/RegisterConfirm.vue
@@ -1,8 +1,29 @@
 <template>
-  <div class="section registerConfirm">
-    <b-icon id="iconConfirm" pack="fas" icon="check-circle" size="is-large" ></b-icon>
-    <p>{{$t('register.confirm')}}</p>
-    <router-link id="btnGoHome" to="/" class="button">{{$t('register.goHome')}}</router-link>
+  <div class="registerConfirm">
+    <div class="section">
+      <b-icon id="iconConfirm" pack="fas" icon="check-circle" size="is-large" ></b-icon>
+      <p>{{$t('register.confirm')}}</p>
+      <router-link id="btnGoHome" to="/" class="button">{{$t('register.goHome')}}</router-link>
+    </div>
+    <div class="section">
+      <div class="container level is-mobile">
+        <a href="https://www.instagram.com/birdlife_insta/" class="item">
+          <span class="icon is-large">
+            <i class="fab fa-instagram fa-3x"></i>
+          </span>
+        </a>
+        <a href="https://www.facebook.com/BirdLifeSuisse/" class="item">
+          <span class="icon is-large">
+            <i class="fab fa-facebook fa-3x"></i>
+          </span>
+        </a>
+        <a href="https://twitter.com/SVS_BirdLife" class="item">
+          <span class="icon is-large">
+            <i class="fab fa-twitter fa-3x"></i>
+          </span>
+        </a>
+      </div>
+    </div>
   </div>
 </template>
 
